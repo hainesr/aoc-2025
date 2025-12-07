e34306e1074a88087d8f0f578939fccd8e1bfdf6
Robert Haines <rhaines@manchester.ac.uk>
2025-12-07 01:37:56 +0000

    Solution for day 6, part 2.

--- lib/aoc2025/days/trash_compactor.rb
+++ lib/aoc2025/days/trash_compactor.rb
@@ -18,6 +18,10 @@ module AOC2025
       calculate(parse_part1(@lines))
     end
 
+    def part2
+      calculate(parse_part2(@lines))
+    end
+
     def parse_part1(lines)
       columns = []
 
@@ -27,6 +31,34 @@ module AOC2025
           num = cell.to_i
           columns[index] << (num.zero? ? cell.to_sym : num)
         end
+      end
+
+      columns
+    end
+
+    def parse_part2(lines)
+      columns = []
+      index = 0
+      num_max = lines.length - 1
+      lines = lines.map(&:reverse)
+
+      while index < lines.first.length
+        operator = nil
+        column = []
+        (0..4).each do |i|
+          num = (0..num_max).map do |line|
+            lines[line][index + i]
+          end
+
+          break if num.all?(' ') || num.all?(&:nil?)
+
+          operator = num.pop.to_sym if num[num_max] != ' '
+          column << num.join.to_i
+        end
+
+        column << operator unless operator.nil?
+        columns << column
+        index += column.length
       end
 
       columns
--- test/days/trash_compactor_test.rb
+++ test/days/trash_compactor_test.rb
@@ -10,12 +10,7 @@ require 'test_helper'
 require 'aoc2025/days/trash_compactor'
 
 class AOC2025::TrashCompactorTest < Minitest::Test
-  INPUT = <<~EOI
-    123 328  51 64
-     45 64  387 23
-      6 98  215 314
-    *   +   *   +
-  EOI
+  INPUT = "123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  \n"
 
   def setup
     @tc = AOC2025::TrashCompactor.new
@@ -34,9 +29,28 @@ class AOC2025::TrashCompactorTest < Minitest::Test
     assert_equal(expected_columns, @tc.parse_part1(@tc.instance_variable_get(:@lines)))
   end
 
+  def test_parse_part2
+    @tc.setup(INPUT)
+
+    expected_columns = [
+      [4, 431, 623, :+],
+      [175, 581, 32, :*],
+      [8, 248, 369, :+],
+      [356, 24, 1, :*]
+    ]
+
+    assert_equal(expected_columns, @tc.parse_part2(@tc.instance_variable_get(:@lines)))
+  end
+
   def test_part1
     @tc.setup(INPUT)
 
     assert_equal(4_277_556, @tc.part1)
+  end
+
+  def test_part2
+    @tc.setup(INPUT)
+
+    assert_equal(3_263_827, @tc.part2)
   end
 end
